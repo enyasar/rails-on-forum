eae0fef561515127acb5712136dfc17717cd2462
Erhan YASAR <erhanyasar35@gmail.com>
2014-04-03 18:54:19 +0300

    Profil sayfasındaki #edit ve #destroy metodları kısıtlandı

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :select_user, only: [:show, :edit, :update, :destroy]
+  before_action :allowed?, only: [:edit, :create, :destroy]
 
   def new
     @user = User.new
@@ -53,5 +54,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit!
+  end
+
+  def allowed?
+    user = select_user
+
+    unless current_user == user
+      redirect_to profile_path(user), alert: "Bunu yapmaya yetkiniz yok!"
+    end
   end
 end
